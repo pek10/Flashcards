e550f18203b34084490ffe809f2820704a03c8ed
Alex Myers <alex@pek10.dev>
2023-02-07 06:02:10 -0500

    Refactored

---
+++ src/components/Cards/EditCard/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Breadcrumb from './Breadcrumb'
+import CardForm from './../CardForm'
+
+function EditCard({ deck }) {
+  return (
+    <>
+      <Breadcrumb deck={deck} />
+      <h1>Edit Card</h1>
+      <CardForm deck={deck} />
+    </>
+  )
+}
+
+export default EditCard
