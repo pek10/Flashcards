5fb27602243203abacfdc3a86f39937438440b75
Alex Myers <alex@pek10.dev>
2023-02-07 06:03:06 -0500

    Refactored home

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,13 +2,8 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Header from './Header'
 import NotFound from './NotFound'
-import Home from '../components/Home'
-import CreateDeck from '../components/CreateDeck'
-import Deck from '../components/Deck'
-import Study from '../components/Study'
-import EditDeck from '../components/EditDeck'
-import AddCard from '../components/AddCard'
-import EditCard from '../components/EditCard'
+import Home from './../components/Home/index'
+import Decks from './../components/Decks/index'
 
 function Layout() {
   return (
@@ -22,28 +17,12 @@ function Layout() {
           >
             <Home />
           </Route>
-          <Route path='/decks/new'>
-            <CreateDeck />
+          <Route path='/decks'>
+            <Decks />
           </Route>
-          <Route
-            exact
-            path='/decks/:deckId'
-          >
-            <Deck />
+          <Route>
+            <NotFound />
           </Route>
-          <Route path='/decks/:deckId/study'>
-            <Study />
-          </Route>
-          <Route path='/decks/:deckId/edit'>
-            <EditDeck />
-          </Route>
-          <Route path='/decks/:deckId/cards/new'>
-            <AddCard />
-          </Route>
-          <Route path='/decks/:deckId/cards/:cardId/edit'>
-            <EditCard />
-          </Route>
-          <NotFound />
         </Switch>
       </div>
     </>
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateDeckButton from './CreateDeckButton'
+import ShowDecks from './ShowDecks'
+
+function Home() {
+  return (
+    <div className='container'>
+      <CreateDeckButton />
+      <ShowDecks />
+    </div>
+  )
+}
+
+export default Home
