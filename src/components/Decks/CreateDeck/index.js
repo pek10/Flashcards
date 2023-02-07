fd3455af450a383c600e180e756ac72752e0ad1c
Alex Myers <alex@pek10.dev>
2023-02-07 06:02:49 -0500

    Refactored Decks

---
+++ src/components/Decks/CreateDeck/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DeckForm from './../DeckForm'
+import Breadcrumb from './Breadcrumb'
+
+function CreateDeck() {
+  return (
+    <>
+      <Breadcrumb />
+      <h1>Create Deck</h1>
+      <DeckForm />
+    </>
+  )
+}
+
+export default CreateDeck
